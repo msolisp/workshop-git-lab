88e073b080ce461c95d0801090eb119bf990599f
Nisum <nisum@NI-NTB-120.local>
2020-08-21 16:33:15 -0400

    first commit

---
+++ src/test/java/com/example/demo/DemoGitLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
